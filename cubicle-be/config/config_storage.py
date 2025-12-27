926e1df94f307a852893d60673b6e841112a1aca
Elyes SADDEM <saddemelyes67@gmail.com>
2025-12-27 13:44:12 +0100

    fix/azure cloud storage doesnt work because of non referenced env

--- cubicle-be/config/config_storage.py
+++ cubicle-be/config/config_storage.py
@@ -5,7 +5,7 @@ ENV = os.getenv("FLASK_ENV", "development")
 if ENV == "production":
     ACCOUNT_NAME = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
     ACCOUNT_KEY = os.getenv("AZURE_STORAGE_ACCOUNT_KEY")
-    CONTAINER_NAME = os.getenv("AZURE_STORAGE_CONTAINER_NAME", "images")
+    CONTAINER_NAME = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
 
     AZURE_STORAGE_CONNECTION_STRING = (
         f"DefaultEndpointsProtocol=https;"
@@ -14,6 +14,7 @@ if ENV == "production":
         f"EndpointSuffix=core.windows.net"
     )
     AZURE_STORAGE_CONTAINER = CONTAINER_NAME
+    AZURE_STORAGE_ACCOUNT_NAME = ACCOUNT_NAME
 else:
     # Local dev folder
     LOCAL_UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads", "avatars")
