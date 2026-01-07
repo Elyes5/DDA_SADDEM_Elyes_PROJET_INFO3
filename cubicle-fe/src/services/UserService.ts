f0a4f5a9d73712d37585e3ddb0ca434fc97d326b
Elyes SADDEM <saddemelyes67@gmail.com>
2026-01-07 23:58:43 +0100

    fix(cubicle-be) : fix api urls

--- cubicle-fe/src/services/UserService.ts
+++ cubicle-fe/src/services/UserService.ts
@@ -5,34 +5,34 @@ import type MessageResponse from '../interfaces/MessageResponse'
 export const userService = {
 
   getUserProfile: async (userId: number): Promise<User> => {
-    const { data } = await api.get<User>(`/api/users/${userId}/`)
+    const { data } = await api.get<User>(`/api/users/${userId}`)
     return data
   },
 
 
   followUser: async (userId: number): Promise<MessageResponse> => {
-    const { data } = await api.post<MessageResponse>(`/api/users/${userId}/follow/`)
+    const { data } = await api.post<MessageResponse>(`/api/users/${userId}/follow`)
     return data
   },
 
   unfollowUser: async (userId: number): Promise<MessageResponse> => {
-    const { data } = await api.post<MessageResponse>(`/api/users/${userId}/unfollow/`)
+    const { data } = await api.post<MessageResponse>(`/api/users/${userId}/unfollow`)
     return data
   },
 
   getFollowers: async (userId: number): Promise<User[]> => {
-    const { data } = await api.get<User[]>(`/api/users/${userId}/followers/`)
+    const { data } = await api.get<User[]>(`/api/users/${userId}/followers`)
     return data
   },
 
   getFollowing: async (userId: number): Promise<User[]> => {
-    const { data } = await api.get<User[]>(`/api/users/${userId}/following/`)
+    const { data } = await api.get<User[]>(`/api/users/${userId}/following`)
     return data
   },
   
   updateProfile: async (formData: FormData): Promise<User> => {
   const { data } = await api.put<{ message: string; user: User }>(
-    '/api/auth/edit/', 
+    '/api/auth/edit', 
     formData,
     { headers: { 'Content-Type': 'multipart/form-data' } }
   )
